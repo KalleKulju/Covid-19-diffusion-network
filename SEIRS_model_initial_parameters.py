9e75af713078131e17268eea1e7da5ab93ea4e91
joelryt <rytkonenjoel@gmail.com>
2022-05-04 20:24:50 +0300

    SEIRS model with initial parameters

---
+++ SEIRS_model_initial_parameters.py
@@ -0,0 +1,26 @@
+import networkx as nx
+import numpy as np
+from seirsplus.models import *
+from seirsplus.networks import *
+
+# Network Definition
+network_size = 5500000
+scaling = 0.001
+g = nx.erdos_renyi_graph(int(network_size * scaling), 0.001)
+
+# Initial model parameter values
+SIGMA  = 1/5.2
+GAMMA  = 1/10
+R0     = 2.5
+BETA   = 1/(1/GAMMA) * R0
+model = SEIRSNetworkModel(G       = g,
+                          beta    = BETA,
+                          sigma   = SIGMA,
+                          gamma   = GAMMA,
+                          initI   = network_size*scaling/100)
+
+# Simulation
+model.run(T=120)
+
+# Visualization
+model.figure_infections(plot_percentages=False)
